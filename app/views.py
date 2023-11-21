695b23f5587bd6849455a3272721a152a87e6b56
pree199 <nayakpreeti111@gmail.com>
2023-11-21 11:42:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def preeti(request):
+    return render(request,'preeti.html')
+
+def varada(request):
+    return render(request,'varada.html')
+
+def vishnu(request):
+    return render(request,'vishnu.html')
+
